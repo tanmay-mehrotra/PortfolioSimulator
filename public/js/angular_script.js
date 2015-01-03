47c55f52aadf319bc690a329871fd03a90df96e1
Tanmay Mehrotra <tanmaymehrotra86@gmail.com>
2015-01-03 04:48:48 -0500

    revamped the design to be more readible and maintainable

--- public/js/angular_script.js
+++ public/js/angular_script.js
@@ -1,15 +1,17 @@
 (function(){
     'use strict';
-    angular.module('portfolioSim', ['ui.bootstrap','nvd3','checklist-model','ng-fusioncharts'])
+    angular.module('portfolioSim', ['ui.bootstrap','googlechart'])
     	.controller('portfolioSimCtrl', ['$scope', '$rootScope', '$http', function($scope, $rootScope, $http) {
     		//parameters needed to run the simulation
 		    $scope.start_date = null;
 	        $scope.end_date = null;
 	        $scope.investment_type = null;
-	        $scope.sectors = [];
-	        $scope.products = [];
-	        $scope.selectedCheckBoxes = [];
+	        $scope.sectors = sectors;
+	        $scope.products = products;
+	        $scope.product_checkBox_counter = 0;
+	        $scope.sector_checkBox_counter = 0;
 
+	        /*
 	        //populate product and sectors array creating 2D array
 			for (var i = 0 ; i< products.length; i += 5) {
     			$scope.products.push(products.slice(i, i + 5));
@@ -17,7 +19,7 @@
 
 			for (var i = 0 ; i< sectors.length; i += 5) {
     			$scope.sectors.push(sectors.slice(i, i + 5));
-			}
+			}*/
 
 	        //list listerns to update parent controller if there is some change in child controllers
 	        $scope.$on("update_parent_controller_start_date", function(event, start_date) {
@@ -28,23 +30,27 @@
         		$scope.end_date = end_date;
       		});
 
-      		//function to flush all the selected Check boxes
-    	   	$scope.clearSelectedCheckBoxes = function(){
-    	   		while($scope.selectedCheckBoxes.length > 0) {
-    				$scope.selectedCheckBoxes.pop();
-				}   	   		
+    	   	$scope.count_product_checkbox = function(product){
+    	   		//console.log(product.key + product.check);
+    	   		if(product.check == 0){
+    	   			$scope.product_checkBox_counter -= 1;
+    	   		}else{
+    	   			$scope.product_checkBox_counter += 1;
+    	   		}
+    	   		for(var i = 0;i<products.length;i++){
+    	   			console.log(products[i].key + " " + products[i].value + " " + products[i].check);
+    	   		}
+    	   		console.log($scope.product_checkBox_counter);
     	   	};
-      		
-		    //when submitting the add form, send the text to the node RestFulAPI
-		    $scope.simulate = function() {
-		        $http.post('/api/calculateDiff', $scope.dates)
-		            .success(function(data) {
-		                $scope.diff = data;
-		            })
-		            .error(function(data) {
-		                console.log('Error: ' + data);
-		            });
-		    };
+
+    	   	$scope.count_sector_checkbox = function(sector){
+    	   		//console.log(product.key + product.check);
+    	   		if(sector.check == 0){
+    	   			$scope.sector_checkBox_counter -= 1;
+    	   		}else{
+    	   			$scope.sector_checkBox_counter += 1;
+    	   		}
+    	   	};
 		}])
 		.controller('datePickerCtrl', ['$scope', '$rootScope', function($scope, $rootScope) {
 		    $scope.start_dt = new Date();
@@ -77,80 +83,94 @@
 
 		    $scope.format = 'dd-MMMM-yyyy'; 
 		}])
-		/*.controller('PieChartCtrl', function($scope){
-			$scope.$watch(function(scope){return $scope.selectedCheckBoxes.length}, 
-      			function(newValue, oldValue){
-					$scope.options = {
-			            chart: {
-			                type: 'pieChart',
-			                height: 300,
-			                x: function(d){return d.name;},
-			                y: function(d){return d.total;},
-			                showLabels: false,
-			                transitionDuration: 500,
-			                showLegend : false,
-			                tooltips: true,
-			                donut: false,
-			            },
-			            css: {
-			              textAlign: "center",
-			              "font-weight": "bold",
-			            }
-		        	};
-		        	var data = [];
-		        	for(var i = 0 ; i < $scope.selectedCheckBoxes.length; i++){
-		        		data.push({name:$scope.selectedCheckBoxes[i], total:100/$scope.selectedCheckBoxes.length});
-		        	}
-			        $scope.data = data;
-	    		}
-	    	);
-		});*/
 		.controller('PieChartCtrl', function($scope){
-			//$scope.$watch(function(scope){return $scope.selectedCheckBoxes.length}, 
-      			//function(newValue, oldValue){
-      				/*var data = [];
-		        	for(var i = 0 ; i < $scope.selectedCheckBoxes.length; i++){
-		        		data.push({label:$scope.selectedCheckBoxes[i], value:100/$scope.selectedCheckBoxes.length});
-		        	}*/
-					$scope.dataSource = {
-			            chart: {
-			                caption: "Age profile of website visitors",
-			                subcaption: "Last Year",
-			                startingangle: "120",
-			                showlabels: "0",
-			                showlegend: "1",
-			                enablemultislicing: "0",
-			                slicingdistance: "15",
-			                showpercentvalues: "1",
-			                showpercentintooltip: "0",
-			                plottooltext: "Age group : $label Total visit : $datavalue",
-			                theme: "ocean"
-            			},
-			            data: [
-			                {
-			                    label: "Teenage",
-			                    value: "1250400"
-			                },
-			                {
-			                    label: "Adult",
-			                    value: "1463300"
-			                },
-			                {
-			                    label: "Mid-age",
-			                    value: "1050700"
-			                },
-			                {
-			                    label: "Senior",
-			                    value: "491000"
-			                }
-            			]
-		        	};
-	    		//}
-	   		 //);
-		});
+			var chartData = [['Component', 'cost']];
 
-	var sectors = ['Automobile', 'Banking', 'FMCG', 'IT', 'Power'];
-	var products = ['HERO', 'MARUTI', 'TATA', 'ICICI', 'IDBI', 'SBI', 'BRITANNIA', 'DABUR', 'GILLETTE',
-						'HCL', 'INFOSYS', 'TCS', 'NTPC', 'RELIANCEINFRA', 'TATAPOWER'];
+			//call when checkbox state is changed --> reset pie chart
+			var checkbox_count_changes = function(){
+			 	//clear chartData -- > we cannot unnecessarily create new objects eveytime
+    	   		while(chartData.length > 1) {
+    				chartData.pop();
+				}   	   		
+    	   	
+			    if($scope.investment_type == 'product_wise'){
+			    	for(var i=0;i<products.length;i++){
+			    		if(products[i].check == 1){
+			    			products[i].value = 100/$scope.product_checkBox_counter;
+			    		}else{
+			    			products[i].value = 0;
+			    		}
+			    		chartData.push([products[i].key,products[i].value]);
+			    	}
+			    }
+
+			    if($scope.investment_type == 'sector_wise'){
+			    	for(var i=0;i<sectors.length;i++){
+			    		if(sectors[i].check == 1){
+			    			sectors[i].value = 100/$scope.sector_checkBox_counter;
+			    		}else{
+			    			sectors[i].value = 0;
+			    		}
+			    		chartData.push([sectors[i].key,sectors[i].value]);
+			    	}
+			    }
+			    render_chart();
+			};
+
+			var investment_type_changes = function(){
+				while(chartData.length > 1) {
+    				chartData.pop();
+				}   	   		
+    	   	
+			    if($scope.investment_type == 'product_wise'){
+			    	for(var i=0;i<products.length;i++){
+			    		chartData.push([products[i].key,products[i].value]);
+			    	}
+			    }
+
+			    if($scope.investment_type == 'sector_wise'){
+			    	for(var i=0;i<sectors.length;i++){
+			    		chartData.push([sectors[i].key,sectors[i].value]);
+			    	}
+			    }
+			    render_chart();
+			}
+
+		    var render_chart =  function(){
+	        	$scope.chart = {
+	        		type : "PieChart",
+	        		data : chartData,
+	        		options :{
+	        			displayExactValues: true,
+				        width: 300,
+				        height: 150,
+				        is3D: true,
+				        chartArea: {left:75,top:10,bottom:0,right:0,height:"100%"}
+	        		},
+	        		formatters : {
+	        			number : [{
+					        columnNum: 1,
+					        pattern: "$ #,##0.00"
+	      				}]
+	        		}
+	        	};
+        	}
+	    
+		    $scope.$watch(function(scope){return ($scope.product_checkBox_counter + $scope.sector_checkBox_counter)}, 
+		    	                          checkbox_count_changes);
+		    $scope.$watch(function(scope){return $scope.investment_type},investment_type_changes);
+		});
+            	
+	
+	var products = [
+	                 	{key:'HERO',value:0, check:0}, {key:'MARUTI',value:0,check:0}, {key:'TATA',value:0,check:0},
+	 				    {key:'ICICI',value:0,check:0}, {key:'IDBI',value:0,check:0}, {key:'SBI',value:0,check:0}, 
+	 				    {key:'BRITANNIA',value:0, check:0}, {key:'DABUR',value:0, check:0}
+	 			   ];
+
+	var sectors = [
+	                   {key:'Automobile',value:0,check:0}, {key:'Banking',value:0,check:0}, {key:'FMCG',value:0,check:0}, 
+	                   {key:'IT',value:0,check:0},{key:'Power',value:0,check:0}
+	              ]; 			   
 
 }());
