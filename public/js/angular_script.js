72c4a09e781c8b663724393a3e9cf28c63b8add9
Tanmay Mehrotra <tanmaymehrotra86@gmail.com>
2015-01-05 01:38:02 -0500

    replaced checkboxes with dropdown and aligned elements using bootstrap

--- public/js/angular_script.js
+++ public/js/angular_script.js
@@ -6,23 +6,12 @@
 		    $scope.start_date = null;
 	        $scope.end_date = null;
 	        $scope.investment_type = null;
-	        $scope.sectors = [];
-	        $scope.products = [];
-	        $scope.product_checkBox_counter = 0;
-	        $scope.sector_checkBox_counter = 0;
+	        $scope.sectors = sectors;
+	        $scope.products = products;
+	        $scope.selectedProducts = [];
+	        $scope.selectedSectors = [];
 	        $scope.slider_changed = 0;
 	        $scope.amount = 0;
-
-	        
-	        //populate product and sectors array creating 2D array
-	        //step indicate number of columns
-			for (var i = 0 ; i< products.length; i += 5) {
-    			$scope.products.push(products.slice(i, i + 5));
-			}
-
-			for (var i = 0 ; i< sectors.length; i += 5) {
-    			$scope.sectors.push(sectors.slice(i, i + 5));
-			}
 
 	        //list listerns to update parent controller if there is some change in child controllers
 	        $scope.$on("update_parent_controller_start_date", function(event, start_date) {
@@ -33,33 +22,14 @@
         		$scope.end_date = end_date;
       		});
 
-    	   	$scope.count_product_checkbox = function(product){
-    	   		//console.log(product.key + product.check);
-    	   		if(product.check == 0){
-    	   			$scope.product_checkBox_counter -= 1;
-    	   		}else{
-    	   			$scope.product_checkBox_counter += 1;
-    	   		}
-    	   	};
-
-    	   	$scope.count_sector_checkbox = function(sector){
-    	   		//console.log(product.key + product.check);
-    	   		if(sector.check == 0){
-    	   			$scope.sector_checkBox_counter -= 1;
-    	   		}else{
-    	   			$scope.sector_checkBox_counter += 1;
-    	   		}
-    	   	};
-
     	   	$scope.change_slider = function(){
-    	   		console.log("slider changed");
     	   		if($scope.investment_type == "product_wise"){
-    	   			for(var i = 0;i<products.length;i++){
-    	   				console.log(products[i].key + " " + products[i].value + " " + typeof products[i].value+ " "+ products[i].check);
+    	   			for(var i = 0;i< $scope.selectedProducts.length;i++){
+    	   				console.log( $scope.selectedProducts[i].key + " " + $scope.selectedProducts[i].value);
     	   			}
     	   		}else{
-    	   			for(var i = 0;i<sectors.length;i++){
-    	   				console.log(sectors[i].key + " " + sectors[i].value + " " + sectors[i].check);
+    	   			for(var i = 0;i< $scope.selectedSectors.length;i++){
+    	   				console.log($scope.selectedSectors[i].key + " " + $scope.selectedSectors[i].value);
     	   			}
     	   		}
     	   		$scope.slider_changed += 1;
@@ -101,38 +71,29 @@
 			var chartData = [['Component', 'cost']];
 
 			//call when checkbox state is changed --> reset pie chart
-			var checkbox_count_changes = function(oldval, newval){
+			var new_selection = function(oldval, newval){
 				if(oldval === newval){
 					return;
 				}
 			 	//clear chartData -- > we cannot unnecessarily create new objects eveytime
-			 	console.log("checkbox_count_changes kicks in");
+			 	console.log("new_selection");
     	   		while(chartData.length > 1) {
     				chartData.pop();
 				}   	   		
     	   	
 			    if($scope.investment_type == 'product_wise'){
-			    	for(var i=0;i<products.length;i++){
-			    		if(products[i].check == 1){
-			    			products[i].value = 100/$scope.product_checkBox_counter;
-			    		}else{
-			    			products[i].value = 0;
-			    		}
-			    		chartData.push([products[i].key,products[i].value]);
+			    	for(var i=0;i< $scope.selectedProducts.length;i++){
+			    		console.log($scope.selectedProducts[i].key + " " + 100/$scope.selectedProducts.length);
+			    		$scope.selectedProducts[i].value = 100/$scope.selectedProducts.length;
+			    		chartData.push([$scope.selectedProducts[i].key, $scope.selectedProducts[i].value]);
 			    	}
-			    	for(var i = 0;i<products.length;i++){
-    	   				console.log(products[i].key + " " + products[i].value + " " + typeof products[i].value + " " + products[i].check);
-    	   			}
 			    }
 
 			    if($scope.investment_type == 'sector_wise'){
-			    	for(var i=0;i<sectors.length;i++){
-			    		if(sectors[i].check == 1){
-			    			sectors[i].value = 100/$scope.sector_checkBox_counter;
-			    		}else{
-			    			sectors[i].value = 0;
-			    		}
-			    		chartData.push([sectors[i].key,sectors[i].value]);
+			    	for(var i=0;i< $scope.selectedSectors.length;i++){
+			    		console.log($scope.selectedSectors[i].key + " " + 100/$scope.selectedSectors.length);
+			    		$scope.selectedSectors[i].value = 100/$scope.selectedSectors.length;
+			    		chartData.push([$scope.selectedSectors[i].key, $scope.selectedSectors[i].value]);
 			    	}
 			    }
 			    render_chart();
@@ -148,19 +109,18 @@
 				}   	   		
     	   	
 			    if($scope.investment_type == 'product_wise'){
-			    	for(var i=0;i<products.length;i++){
-			    		chartData.push([products[i].key,Number(products[i].value)]);
+			    	for(var i=0;i< $scope.selectedProducts.length;i++){
+			    		chartData.push([$scope.selectedProducts[i].key, Number($scope.selectedProducts[i].value)]);
 			    	}
 			    }
 
 			    if($scope.investment_type == 'sector_wise'){
-			    	for(var i=0;i<sectors.length;i++){
-			    		chartData.push([sectors[i].key,Number(sectors[i].value)]);
+			    	for(var i=0;i< $scope.selectedSectors.length;i++){
+			    		chartData.push([$scope.selectedSectors[i].key, Number($scope.selectedSectors[i].value)]);
 			    	}
 			    }
-			    console.log("re rendering chart");
 			    render_chart();
-			}
+			};
 
 			var slider_changes = function(oldval, newval){
 				investment_type_changes(oldval,newval);
@@ -172,10 +132,10 @@
 	        		data : chartData,
 	        		options :{
 	        			displayExactValues: true,
-				        width: 300,
+				        width: 350,
 				        height: 150,
 				        is3D: true,
-				        chartArea: {left:75,top:10,bottom:0,right:0,height:"100%"}
+				        chartArea: {left:25,top:10,bottom:0,right:0,height:"100%"}
 	        		},
 	        		formatters : {
 	        			number : [{
@@ -186,22 +146,22 @@
 	        	};
         	}
 	    	//watches to trigger re-rendering of chart on change of user input
-		    $scope.$watch(function(scope){return (scope.product_checkBox_counter + scope.sector_checkBox_counter)}, 
-		    	                          checkbox_count_changes);
+		    $scope.$watch(function(scope){return (scope.selectedProducts.length + scope.selectedSectors.length)}, 
+		    	                          new_selection);
 		    $scope.$watch(function(scope){return scope.investment_type},investment_type_changes);
 		    $scope.$watch(function(scope){return scope.slider_changed},slider_changes);
 		});
             	
 	
 	var products = [
-	                 	{key:'HERO',value:0, check:0}, {key:'MARUTI',value:0,check:0}, {key:'TATA',value:0,check:0},
-	 				    {key:'ICICI',value:0,check:0}, {key:'IDBI',value:0,check:0}, {key:'SBI',value:0,check:0}, 
-	 				    {key:'BRITANNIA',value:0, check:0}, {key:'DABUR',value:0, check:0}
+	                 	{key:'HERO',value:0}, {key:'MARUTI',value:0}, {key:'TATA',value:0},
+	 				    {key:'ICICI',value:0}, {key:'IDBI',value:0}, {key:'SBI',value:0}, 
+	 				    {key:'BRITANNIA',value:0}, {key:'DABUR',value:0}
 	 			   ];
 
 	var sectors = [
-	                   {key:'Automobile',value:0,check:0}, {key:'Banking',value:0,check:0}, {key:'FMCG',value:0,check:0}, 
-	                   {key:'IT',value:0,check:0},{key:'Power',value:0,check:0}
-	              ]; 			   
+	                   {key:'Automobile',value:0}, {key:'Banking',value:0}, {key:'FMCG',value:0}, 
+	                   {key:'IT',value:0}, {key:'Power',value:0}
+	              ];
 
 }());
